c1ee9f18fe2ac028e25f6255930fa8af39f68b9a
Anton Staroverov <73071319+tonystar@users.noreply.github.com>
2026-05-13 16:28:24 -0600

    v2.2.1
    fixes:
    https://github.com/picocss/pico/issues/733
    https://github.com/picocss/pico/issues/734
    
    includes:
    Blades standalone v2.2.0

--- css/postcss.config.js
+++ css/postcss.config.js
@@ -4,7 +4,17 @@ module.exports = {
     autoprefixer: {
       cascade: false,
     },
-    "postcss-import": {},
+    "postcss-import": {
+      //TODO: add to tricks
+      // This forces the use of Node's resolution logic which respects 'exports'
+      resolve: (id, basedir) => {
+        try {
+          return require.resolve(id, { paths: [basedir] });
+        } catch (e) {
+          return id;
+        }
+      },
+    },
     "postcss-nested": {},
   },
 };
